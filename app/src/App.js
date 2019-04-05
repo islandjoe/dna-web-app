b4e4a72541d2bac354f5ad30bc2e98a9f8166f36
Arthur Kho <islandjoe@gmail.com>
2019-04-05 13:44:47 +0300

    Login component

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {hot} from 'react-hot-loader'
+import Login from './components/Login.js'
+
 import './App.css'
 
 class App extends Component {
@@ -7,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello!</h1>
+        <Login/>
       </div>
     )
   }
