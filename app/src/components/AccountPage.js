ce948c3a7ecd3fcc93101a7a133921450a840c61
Arthur Kho <islandjoe@gmail.com>
2019-04-13 16:28:38 +0300

    Implement initial AccountPage UI

--- app/src/components/AccountPage.js
+++ app/src/components/AccountPage.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Subscriber from './Subscriber'
 import {Link} from  'react-router-dom'
+import {Image, List} from 'semantic-ui-react'
 import {URL, subscribers} from '../data'
 
 const AccountPage =(props)=> {
-  const details  = subscribers.find(s=> s.userid === props.userid)
+  const details = subscribers.find(s=> s.userid === props.userid)
   const bundle = details.subscriptions.map(pkg=>
-    <li key={ pkg.id }>
-      <Link to={ `${URL.subscription}/${pkg.id}` }>
-        { pkg.name }
-      </Link>
-    </li>
+    <List.Item key={ pkg.id }>
+      <Image size='tiny'
+            {/*TODO: Handle if there's no avatar */}
+            src={ `/images/avatar-${pkg.id}.jpeg`}
+            verticalAlign='top' />
+      <List.Content>
+        <Link to={ `${URL.subscription}/${pkg.id}` }>
+          <List.Header>{ pkg.name }</List.Header>
+        </Link>
+      </List.Content>
+    </List.Item>
   )
 
   return (
@@ -18,9 +25,9 @@ const AccountPage =(props)=> {
       <Subscriber userid={ props.userid }/>
       <div>
         <h3>Your subscriptions</h3>
-        <ul>
+        <List>
           { bundle }
-        </ul>
+        </List>
       </div>
 
     </div>
