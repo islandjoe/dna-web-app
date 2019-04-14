8ae5cc1218dbaa2ccf1be2fe3fa7dce06d970894
Arthur Kho <islandjoe@gmail.com>
2019-04-14 10:07:01 +0300

    User ID input validation

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,18 +1,26 @@
 import React, {Component}  from 'react'
 import authenticate from '../Auth'
 import {URL}  from '../data'
-import {Form, Button} from 'semantic-ui-react'
+import {Form, Button, Input} from 'semantic-ui-react'
 
 class Login extends Component {
 
   constructor(props) {
     super(props)
 
-    this.state = { userid:'', password:'' }
+    this.state = {
+      userid:'',
+      password:'',
+      isValid: {
+        email: !true,
+      // isValidPassword: false
+      }
+    }
 
     this.handleSubmit   = this.handleSubmit.bind(this)
     this.handleUserId   = this.handleUserId.bind(this)
     this.handlePassword = this.handlePassword.bind(this)
+    this.validate       = this.validate.bind(this)
   }
 
   handleSubmit(event) {
@@ -28,10 +36,25 @@ class Login extends Component {
     }
   }
 
-  handleUserId(event) { console.log( event )
+  handleUserId(event) {
     const _fromInput = event.target
 
     this.setState({ userid: _fromInput.value })
+  }
+
+  validate(event) {
+    // RFC 5322 Official Standard
+    const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const input = event.target
+
+    //1. userid is email format
+    if ( input.value.length > 0 && validEmail.test( input.value )) {
+      this.setState({ isValid: { email: !true }})
+    }
+    else {
+      this.setState({ isValid: { email: !false }})
+      input.focus()
+    }
   }
 
   handlePassword(event) {
@@ -43,18 +66,25 @@ class Login extends Component {
   render() {
     return (
       <Form className='Login' onSubmit={ this.handleSubmit }>
-        <Form.Field className='UserIdField'>
+        <Form.Field className='UserIdField' error={ this.state.isValid.email }>
           <label>User ID:</label>
-          <input placeholder='email' value={ this.state.userid }
-              onChange={ this.handleUserId }/>
+          <Input placeholder='Email'
+              value={ this.state.userid }
+              onChange={ this.handleUserId }
+              onBlur={ this.validate }
+              />
           </Form.Field>
 
-          <Form.Field className='PasswordField'>
+          <Form.Field className='PasswordField' disabled={ this.state.isValid.email }>
             <label>Password:</label>
-            <input type='password' onChange={ this.handlePassword }/>
+            <Input type='password' onChange={ this.handlePassword }/>
           </Form.Field>
 
-        <Button type='submit' className='LoginButton'>Log In</Button>
+        <Button type='submit'
+              className='LoginButton'
+              disabled={ this.state.isValid.email }>
+          Log In
+        </Button>
       </Form>
     )
   }
