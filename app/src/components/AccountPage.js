12e7b284ed95c344ed398cd6c51bd5557f72c320
Arthur Kho <islandjoe@gmail.com>
2019-04-15 10:34:53 +0300

    Refactoring and cleaning up AccountPage

--- app/src/components/AccountPage.js
+++ app/src/components/AccountPage.js
@@ -6,26 +6,33 @@ import {URL, subscribers} from '../data'
 
 const AccountPage =(props)=> {
   const details = subscribers.find(s=> s.userid === props.userid)
-  const bundle = details.subscriptions.map(pkg=>
-    <List.Item key={ pkg.id }>
+  const packages = details.subscriptions.map(pkg=> (
+      <List.Item key={pkg.id}>
       {/* TODO: Handle if there's no avatar */}
-      <Image size='tiny' src={ `/images/avatar-${pkg.id}.jpeg`}
-            verticalAlign='top' />
+      <Image size='tiny' rounded
+             src={`/images/avatar-${pkg.id}.jpeg`}
+             verticalAlign='top' />
       <List.Content>
-        <Link to={ `${URL.subscription}/${pkg.id}` }>
-          <List.Header>{ pkg.name }</List.Header>
+        <List.Header>
+        <Link to={`${URL.subscription}/${pkg.id}`}>
+          {pkg.name}
         </Link>
+        </List.Header>
+        <List.Description>
+          { pkg.desc }
+        </List.Description>
+
       </List.Content>
     </List.Item>
-  )
+  ))
 
   return (
     <div>
       <Subscriber userid={ props.userid }/>
       <div>
-        <h3>Your subscriptions</h3>
-        <List>
-          { bundle }
+        <h2>Your subscriptions</h2>
+        <List celled relaxed='very' selection size='large'>
+        { packages }
         </List>
       </div>
 
