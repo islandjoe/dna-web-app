4a5c222e7e548d720c55ec36e7d0a5daf85d9a0a
Arthur Kho <islandjoe@gmail.com>
2019-04-15 10:35:41 +0300

    Add 'desc' field to the subscription packages/bundles

--- app/src/data.js
+++ app/src/data.js
@@ -2,15 +2,18 @@ export const packages = [
   {id: 'home-broadband',
    name: 'Home Broadband',
    type: 'Broadband Internet',
+   desc: 'Streaming up to 4K videos, surf the net at the same time',
    price: 28
   },
   {id: '4g-rajaton',
    name: '4G Mobile Rajaton',
+   desc: '100Mbit/s, 20Gt/kk EU/ETA, Rajaton',
    type: 'Mobile',
    price: 26
   },
   {id: 'cable-hd',
    name: 'Cable HD Premium',
+   desc: 'Upt to 4K video, TV Hub, Net TV',
    type: 'Cable',
    price: 35
   },
@@ -21,22 +24,22 @@ export const subscribers = [
     userid:'lord.of.winterfell@geocities.com',
     password: 'wheremyhead?',
     name:'Ned Stark',
-    address:'Castle Winterfell',
-    mobile:'123-4567',
+    address:'North Stead, Castle Winterfell',
+    mobile:'0123-4567',
     subscriptions: [ packages[1] ]
   },{
     userid:'k.slayer67@aol.com',
     password: 'wheremyhand?',
     name:'Jaime Lannister',
-    address:'The Westerlands',
-    mobile:'111-1666',
+    address:'Main St., The Westerlands',
+    mobile:'0111-1666',
     subscriptions: [ packages[0], packages[2] ]
   },{
     userid:'jsnow38@hotmail.com',
     password: 'huh?',
     name:'John Snow',
-    address:'Castle Black',
-    mobile:'100-000',
+    address:'Lord Commander 1 A, Castle Black',
+    mobile:'04435-8888',
     subscriptions: [ ...packages ]
   },
 ]
