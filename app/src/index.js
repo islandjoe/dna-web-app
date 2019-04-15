d87d2d85d62834ab9a33882599aab799cfce0ca5
Arthur Kho <islandjoe@gmail.com>
2019-04-15 08:24:51 +0300

    Remove normalize.css
    Semantic UI comes with its own CSS reset already.

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter} from  'react-router-dom'
 import App from './App.js'
 
-import 'normalize.css'
 import 'semantic-ui-css/semantic.min.css'
 
 const app = (
